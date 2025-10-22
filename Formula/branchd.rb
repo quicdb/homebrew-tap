7cffafb7463d58b268566747e1e4fdce3a9e92f7
Rafael Garcia <rafbgarcia@gmail.com>
2025-10-22 20:49:02 -0300

    Init

---
+++ Formula/branchd.rb
@@ -0,0 +1,33 @@
+class Branchd < Formula
+  desc "Branchd CLI for managing database branches"
+  homepage "https://branchd.dev"
+  version "1.0.6"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/branchd-dev/branchd-cli/releases/download/v1.0.6/branchd-darwin-arm64"
+      sha256 "abe827fb997acbac269170f3175e15ba113955dfc5980e67b45b2ed127f8c3f4"
+    else
+      url "https://github.com/branchd-dev/branchd-cli/releases/download/v1.0.6/branchd-darwin-amd64"
+      sha256 "ad194e7df985f9a06fb0b2290a540388b81f469657caa764a650a4f07c0e5f3e"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/branchd-dev/branchd-cli/releases/download/v1.0.6/branchd-linux-arm64"
+      sha256 "54bb51435ad35c5e7a9d278e7a5ed78ebbc6f96c8c5bde08a7ff07b662ccccf2"
+    else
+      url "https://github.com/branchd-dev/branchd-cli/releases/download/v1.0.6/branchd-linux-amd64"
+      sha256 "321d016300e37fd320b973cf36d8b2f2d0a3ef6393aa8dffbd1709812467c0aa"
+    end
+  end
+
+  def install
+    bin.install Dir["branchd-*"].first => "branchd"
+  end
+
+  test do
+    system "#{bin}/branchd", "version"
+  end
+end
---
+++ README.md
@@ -0,0 +1,22 @@
+# Branchd CLI
+
+Official Homebrew tap for the Branchd CLI.
+
+## Installation
+
+### macOS and Linux
+
+```bash
+brew tap branchd-dev/tap
+brew install branchd
+```
+
+## Updating
+
+```bash
+brew upgrade branchd
+```
+
+## Support
+
+- Issues: https://github.com/branchd-dev/branchd-cli/issues
